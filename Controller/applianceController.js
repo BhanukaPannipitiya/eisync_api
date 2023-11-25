6c81ef9cc25ff9eccdd7a78e8013ab6f483e30a2
Bhanuka69 <103242662+Bhanuka69@users.noreply.github.com>
2023-11-26 02:49:19 +0530

    cost estimation and device view logics added

--- Controller/applianceController.js
+++ Controller/applianceController.js
@@ -1,45 +1,134 @@
 const Appliance = require("../Models/appliance");
+const CostEstimation = require("../Models/costEstimation");
+const Goal = require('../Models/goal');
 
-exports.AddAppliance = async(req, res) => {
-  console.log("first", req.body)
-    const appliance = new Appliance({
-      deviceType: req.body.deviceType,
-      power: req.body.power,      
-      voltage: req.body.voltage,       
-      onHours: req.body.onHours,         
-      deviceModel: req.body.deviceModel,
-      deviceBrand: req.body.deviceBrand,
-      isActive: true,      // Remove quotes
-      createdOn: Date.now(),
-      userId: req.body.userId,
-    });
-    
-    console.log("first", appliance);
-      const savedAppliance = await appliance.save();
-  
-      res.json(savedAppliance);
-  
+exports.AddAppliance = async (req, res) => {
+  console.log("first", req.body);
+  const appliance = new Appliance({
+    deviceType: req.body.deviceType,
+    power: req.body.power,
+    voltage: req.body.voltage,
+    onHours: req.body.onHours,
+    deviceModel: req.body.deviceModel,
+    deviceBrand: req.body.deviceBrand,
+    isActive: true, // Remove quotes
+    createdOn: Date.now(),
+    userId: req.body.userId,
+  });
+
+  console.log("first", appliance);
+  const savedAppliance = await appliance.save();
+
+  res.json(savedAppliance);
 };
 
-// exports.createUser = async (req, res) => {
-//   try {
-//     // Create a new user instance
-//     const user = new User({
-//       email: "BIWA@GMAIL.COM",
-//       name: "bhanuka",
-//       phoneNumber: "0121345687",
-//       isActive: "true",
-//       createdOn: "",
-//       country: "",
-//       loginPassword: "",
-//     });
+//create a endpoint to get all appliances
+exports.getAllAppliances = async (req, res) => {
+  const appliances = await Appliance.find();
+  res.json(appliances);
+};
 
-//     // Save the user to the database
-//     const savedUser = await user.save();
+//create a endpoint to calculate total cost
+exports.calculateTotalCost = async (req, res) => {
+  try {
+    const { devices, fixedUnits, fixPrice } = req.body;
 
-//     res.json(savedUser);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// // }
+    // Calculate the total cost for each device
+    const deviceTotalCosts = devices.map((device) => {
+      const powerInKilowatts = device.power / 1000;
+      const energyConsumed = powerInKilowatts * device.activeHour;
+
+      // Find the applicable unit price based on the total units consumed
+      const applicableUnitPrice = fixedUnits.find(
+        (unit) => energyConsumed >= unit.from && energyConsumed <= unit.to
+      );
+
+      // If a matching unit price is found, use it; otherwise, use a default of 0
+      const unitPrice = applicableUnitPrice
+        ? applicableUnitPrice.costPerUnit
+        : 0;
+
+      // Calculate the total cost for the device, including fixed charges if any
+      const totalCost = energyConsumed * unitPrice + parseFloat(fixPrice || 0);
+
+      return totalCost;
+    });
+
+    // Calculate the overall total bill
+    const totalBill = deviceTotalCosts.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+
+    res.json({ totalBill });
+  } catch (error) {
+    console.error("Error calculating total cost:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+//to save estimations
+exports.saveEstimation = async (req, res) => {
+  try {
+    const {
+      fromDate,
+      toDate,
+      currencyType,
+      estimatedCost,
+      isActive,
+      createdDate,
+      userId,
+    } = req.body;
+
+    // Create a new CostEstimation instance
+    const newEstimation = new CostEstimation({
+      fromDate,
+      toDate,
+      currencyType,
+      estimatedCost,
+      isActive,
+      createdDate,
+      userId,
+    });
+
+    // Save to the database
+    await newEstimation.save();
+
+    res.status(201).json({ message: "Estimation saved successfully" });
+  } catch (error) {
+    console.error("Error saving estimation:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// Create a new goal
+ exports.createGoal = async (req, res) => {
+  try {
+    const {
+      goalName,
+      startDate,
+      endDate,
+      goalAmount,
+      isActive,
+      status,
+      alertOverUsage,
+      userId,
+    } = req.body;
+
+    const goal = new Goal({
+      goalName,
+      startDate,
+      endDate,
+      goalAmount,
+      isActive,
+      status,
+      alertOverUsage,
+      userId,
+    });
+
+    const savedGoal = await goal.save();
+    res.json(savedGoal);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { AddAppliance, createUser } = require("../Controller/applianceController");
+const {
+  AddAppliance,
+  createUser,
+  getAllAppliances,
+  calculateTotalCost,
+  saveEstimation,
+  createGoal,
+} = require("../Controller/applianceController");
 const router = express.Router();
 
 router.post("/addAppliance", AddAppliance);
+router.post("/calculateTotalCost", calculateTotalCost);
+router.get("/getAllAppliances", getAllAppliances);
+router.post("/saveEstimation", saveEstimation);
+router.post("/createGoal", createGoal);
 
 // router.post("/createUser", createUser);
 
